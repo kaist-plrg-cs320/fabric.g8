38679c0212eda6911847d28140078e408ef212c2
Jaemin Hong <hjm0901@gmail.com>
2023-03-20 17:34:13 +0900

    update tests

--- src/main/g8/core/src/test/scala/cs320/SpecBase.scala
+++ src/main/g8/core/src/test/scala/cs320/SpecBase.scala
@@ -4,16 +4,13 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 trait SpecBase extends AnyFlatSpec {
 
-  val len = 200
-
-  def normalize(s: String): String = {
-    val s1 = s.replaceAll("Spec.", "")
+  def normalize(s: String): String =
+    s.replaceAll("Spec.", "")
       .replaceAll("Implementation.", "")
       .replaceAll("Predef.", "")
+      .replaceAll("`package`.", "")
       .replaceAll(".apply", "")
       .replaceAll("\\\\n", " ")
       .replaceAll("\\s+", " ")
       .replaceAll("\\\\'", "'")
-    if (s1.length <= len) s1 else s"${s1.substring(0, len)} ..."
-  }
 }
